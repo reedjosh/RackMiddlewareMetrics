a1c3407c09c70aa2cf070c003cb76914bf26b13b
Reed, Joshua <joshua.reed@intel.com>
2020-11-01 14:48:12 -0800

    Fixed up docs of md5_gen module and defined function better.

--- lib/md5_gen.rb
+++ lib/md5_gen.rb
@@ -1,25 +1,41 @@
 # frozen_string_literal: true
 
 # lib/md5_gen.rb
-#
-# Provide md5 compute function that either uses native extention or Ruby digest lib.
 require 'helix_runtime'
 require('digest/md5')
 
+begin
+  require('md5_ruby_ext/native')
+  EXT_LOADED = true
+rescue LoadError
+  warn('Unable to load md5_ruby_ext/native. Falling back to Ruby digest/md5. ' \
+       'Please run `rake build` to build native extension.')
+  EXT_LOADED = false
+end
+
+# Provides md5 compute function that either uses native extention or Ruby digest lib.
 module MD5Gen
-  begin
-    require('md5_ruby_ext/native')
-    def MD5Gen.compute body
-      MRubyExt.compute(body)
-    # Can only work on UTF-8 strings atm. Helix limitation.
-    rescue TypeError
-      Digest::MD5.hexdigest(body)
+  # Define the underlying_compute_method based upon whether the rust extention is loaded.
+  class << self
+    if EXT_LOADED
+      define_method(:underlying_compute_method) do |body|
+        MRubyExt.compute(body)
+      # Can only work on UTF-8 strings atm. Helix limitation.
+      rescue TypeError
+        Digest::MD5.hexdigest(body)
+      end
+    else
+      define_method(:underlying_compute_method) do |body|
+        Digest::MD5.hexdigest(body)
+      end
     end
-  rescue LoadError
-    warn('Unable to load md5_ruby_ext/native. Falling back to Ruby digest/md5. ' \
-         'Please run `rake build` to build native extension.')
-    def MD5Gen.compute body # rubocop:disable Lint/DuplicateMethods
-      Digest::MD5.hexdigest(body)
-    end
+  end
+
+  # Convert body string to MD5 hash.
+  #
+  # @param body [String] the body to be hashed.
+  # @return [String] the hash of the body.
+  def self.compute body
+    underlying_compute_method(body)
   end
 end
